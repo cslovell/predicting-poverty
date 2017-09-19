8f6f267ca0015c81c638b6fc175130795db71c55
Sean Lovell <csean.lovell@gmail.com>
2017-09-19 14:27:00 +0700

    Fixed links to match updated data

--- scripts/ProcessSurveyData.R
+++ scripts/ProcessSurveyData.R
@@ -1,7 +1,7 @@
 ## PROCESS LSMS AND DHS DATA ##
 
 #### Preliminaries: Load packages, create new folders, define aggregation functions ####
-setwd('predicting-poverty') # Set working directory to where you downloaded the replication folder
+setwd('/home/cslovell/PycharmProjects/predicting-poverty') # Set working directory to where you downloaded the replication folder
 rm(list=ls())
 library(magrittr)
 library(foreign)
@@ -11,9 +11,26 @@ library(readstata13) # One Tanzanian LSMS .dta file was saved in Stata-13 format
 library(plyr)
 '%&%' <- function(x,y)paste0(x,y)
 
+Int2Factor <- function(x)
+{
+  if(!is.null(attr(x, "value.labels"))){
+    vlab <- attr(x, "value.labels")
+    if(sum(duplicated(vlab)) > 0)
+      cat("Duplicated levels:", vlab, "\n")
+    else if(sum(duplicated(names(vlab))) > 0)
+      cat("Duplicated labels:",
+          names(vlab)[duplicated(names(vlab))], "\n")
+    else
+      x <- factor(x, levels = as.numeric(vlab),
+                  labels = names(vlab))
+  }
+  x
+}
 
-dir.create('data/output/LSMS', showWarnings = F)
-dir.create('data/output/DHS', showWarnings = F)
+
+#convert.factors = FALSE
+#dir.create('data/output/LSMS', showWarnings = F)
+#dir.create('data/output/DHS', showWarnings = F)
 
 # Assign each cluster the mean nightlights values over a 10 km^2 area centered on its provided coordinates
 nl <- function(df, year){
@@ -67,14 +84,14 @@ cluster <- function(df, dhs = F){
 #### Write LSMS Data ####
 
 ## Uganda ##
-uga12.cons <- read.dta('data/input/LSMS/UGA_2011_UNPS_v01_M_STATA/UNPS 2011-12 Consumption Aggregate.dta') %$%
+uga12.cons <- read.dta('data/input/LSMS/UGA_2011_UNPS_v01_M_Stata/UNPS 2011-12 Consumption Aggregate.dta') %$%
   data.frame(hhid = HHID, cons = welfare*118.69/(30*946.89*mean(c(66.68, 71.55))))
-uga12.geo <- read.dta('data/input/LSMS/UGA_2011_UNPS_v01_M_STATA/UNPS_Geovars_1112.dta')
+uga12.geo <- read.dta('data/input/LSMS/UGA_2011_UNPS_v01_M_Stata/UNPS_Geovars_1112.dta')
 uga12.coords <- data.frame(hhid = uga12.geo$HHID, lat = uga12.geo$lat_mod, lon = uga12.geo$lon_mod)
 uga12.rururb <- data.frame(hhid = uga12.geo$HHID, rururb = uga12.geo$urban, stringsAsFactors = F)
-uga12.weight <- read.dta('data/input/LSMS/UGA_2011_UNPS_v01_M_STATA/GSEC1.dta')[,c('HHID', 'mult')]
+uga12.weight <- read.dta('data/input/LSMS/UGA_2011_UNPS_v01_M_Stata/GSEC1.dta')[,c('HHID', 'mult')]
 names(uga12.weight) <- c('hhid', 'weight')
-uga12.hh9 <- read.dta('data/input/LSMS/UGA_2011_UNPS_v01_M_STATA/GSEC9A.dta')
+uga12.hh9 <- read.dta('data/input/LSMS/UGA_2011_UNPS_v01_M_Stata/GSEC9A.dta')
 uga12.room <- data.frame(hhid = uga12.hh9$HHID, room = uga12.hh9$h9q3)
 uga12.metal <- data.frame(hhid = uga12.hh9$HHID, roof = uga12.hh9$h9q4=='Iron sheets')
 
@@ -86,16 +103,16 @@ write.table(uga12.vars, 'data/output/LSMS/Uganda 2012 LSMS (Household).txt', row
 write.table(cluster(uga12.vars), 'data/output/LSMS/Uganda 2012 LSMS (Cluster).txt', row.names = F)
 
 ## Tanzania ##
-tza13.cons <- read.dta('data/input/LSMS/TZA_2012_LSMS_v01_M_STATA_English_labels/ConsumptionNPS3.dta') %$%
+tza13.cons <- read.dta('data/input/LSMS/TZA_2012_NPS-R3_v01_M_STATA_English_labels/ConsumptionNPS3.dta') %$%
   data.frame(hhid = y3_hhid, cons = expmR/(365*adulteq))
 tza13.cons$cons <- tza13.cons$cons*112.69/(585.52*mean(c(130.72,141.01)))
-tza13.geo <- read.dta13('data/input/LSMS/TZA_2012_LSMS_v01_M_STATA_English_labels/HouseholdGeovars_Y3.dta')
+tza13.geo <- read.dta13('data/input/LSMS/TZA_2012_NPS-R3_v01_M_STATA_English_labels/HouseholdGeovars_Y3.dta')
 tza13.coords <- data.frame(hhid = tza13.geo$y3_hhid, lat = tza13.geo$lat_dd_mod, lon = tza13.geo$lon_dd_mod)
-tza13.hha <- read.dta('data/input/LSMS/TZA_2012_LSMS_v01_M_STATA_English_labels/HH_SEC_A.dta')
+tza13.hha <- read.dta('data/input/LSMS/TZA_2012_NPS-R3_v01_M_STATA_English_labels/HH_SEC_A.dta')
 tza13.rururb <- data.frame(hhid = tza13.hha$y3_hhid, rururb = tza13.hha$y3_rural, stringsAsFactors = F)
-tza13.weight <- read.dta('data/input/LSMS/TZA_2012_LSMS_v01_M_STATA_English_labels/HH_SEC_A.dta')[,c('y3_hhid', 'y3_weight')]
+tza13.weight <- read.dta('data/input/LSMS/TZA_2012_NPS-R3_v01_M_STATA_English_labels/HH_SEC_A.dta')[,c('y3_hhid', 'y3_weight')]
 names(tza13.weight) <- c('hhid', 'weight')
-tza13.hhi <- read.dta('data/input/LSMS/TZA_2012_LSMS_v01_M_STATA_English_labels/HH_SEC_I.dta')
+tza13.hhi <- read.dta('data/input/LSMS/TZA_2012_NPS-R3_v01_M_STATA_English_labels/HH_SEC_I.dta')
 tza13.room <- na.omit(data.frame(hhid = tza13.hhi$y3_hhid, room = tza13.hhi$hh_i07_1))
 tza13.metal <- data.frame(hhid = tza13.hhi$y3_hhid, metal = tza13.hhi$hh_i09=='METAL SHEETS (GCI)')
 
@@ -107,14 +124,14 @@ write.table(tza13.vars, 'data/output/LSMS/Tanzania 2013 LSMS (Household).txt', r
 write.table(cluster(tza13.vars), 'data/output/LSMS/Tanzania 2013 LSMS (Cluster).txt', row.names = F)
 
 ## Malawi ##
-mwi13.cons <- read.dta('data/input/LSMS/MWI_2013_IHPS_v01_M_STATA/Round 2 (2013) Consumption Aggregate.dta') %$%
+mwi13.cons <- read.dta('data/input/LSMS/MWI_2013_IHPS_v01_M_Stata/Round 2 (2013) Consumption Aggregate.dta') %$%
   data.frame(hhid = y2_hhid, cons = rexpagg/(365*adulteq), weight = hhweight)
 mwi13.cons$cons <- mwi13.cons$cons*107.62/(116.28*166.12)
-mwi13.geo <- read.dta('data/input/LSMS/MWI_2013_IHPS_v01_M_STATA/Geovariables/HouseholdGeovariables_IHPS.dta')
+mwi13.geo <- read.dta('data/input/LSMS/MWI_2013_IHPS_v01_M_Stata/HouseholdGeovariables_IHPS.dta', convert.factors = FALSE)
 mwi13.coords <- data.frame(hhid = mwi13.geo$y2_hhid, lat = mwi13.geo$LAT_DD_MOD, lon = mwi13.geo$LON_DD_MOD)
-mwi13.hha <- read.dta('data/input/LSMS/MWI_2013_IHPS_v01_M_STATA/Household/HH_MOD_A_FILT.dta')
+mwi13.hha <- read.dta('data/input/LSMS/MWI_2013_IHPS_v01_M_Stata/HH_MOD_A_FILT.dta')
 mwi13.rururb <- data.frame(hhid = mwi13.hha$y2_hhid, rururb = mwi13.hha$baseline_rural, stringsAsFactors = F)
-mwi13.hhf <- read.dta('data/input/LSMS/MWI_2013_IHPS_v01_M_STATA/Household/HH_MOD_F.dta')
+mwi13.hhf <- read.dta('data/input/LSMS/MWI_2013_IHPS_v01_M_Stata/HH_MOD_F.dta')
 mwi13.room <- data.frame(hhid = mwi13.hhf$y2_hhid, room = mwi13.hhf$hh_f10)
 mwi13.metal <- data.frame(hhid = mwi13.hhf$y2_hhid, metal = mwi13.hhf$hh_f10=='IRON SHEETS')
 
@@ -129,12 +146,12 @@ write.table(cluster(mwi13.vars), 'data/output/LSMS/Malawi 2013 LSMS (Cluster).tx
 nga13.cons <- read.dta('data/input/LSMS/DATA/cons_agg_w2.dta') %$%
   data.frame(hhid = hhid, cons = pcexp_dr_w2/365)
 nga13.cons$cons <- nga13.cons$cons*110.84/(79.53*100)
-nga13.geo <- read.dta('data/input/LSMS/DATA/Geodata Wave 2/NGA_HouseholdGeovars_Y2.dta')
+nga13.geo <- read.dta('data/input/LSMS/DATA/Geodata Wave 2/NGA_HouseholdGeovars_Y2.dta', convert.factors = FALSE)
 nga13.coords <- data.frame(hhid = nga13.geo$hhid, lat = nga13.geo$LAT_DD_MOD, lon = nga13.geo$LON_DD_MOD)
 nga13.rururb <- data.frame(hhid = nga13.geo$hhid, rururb = nga13.geo$sector, stringsAsFactors = F)
-nga13.weight <- read.dta('data/input/LSMS/DATA/HHTrack.dta')[,c('hhid', 'wt_wave2')]
+nga13.weight <- read.dta('data/input/LSMS/DATA/HHTrack.dta', convert.factors = FALSE)[,c('hhid', 'wt_wave2')]
 names(nga13.weight)[2] <- 'weight'
-nga13.phhh8 <- read.dta('data/input/LSMS/DATA/Post Harvest Wave 2/Household/sect8_harvestw2.dta')
+nga13.phhh8 <- read.dta('data/input/LSMS/DATA/Post Harvest Wave 2/Household/sect8_harvestw2.dta', convert.factors = FALSE)
 nga13.room <- data.frame(hhid = nga13.phhh8$hhid, room = nga13.phhh8$s8q9)
 nga13.metal <- data.frame(hhid = nga13.phhh8$hhid, metal = nga13.phhh8$s8q7=='IRON SHEETS')
 
@@ -184,7 +201,7 @@ write.table(tza10.dhs, 'data/output/DHS/Tanzania 2010 DHS (Household).txt', row.
 write.table(cluster(tza10.dhs, T), 'data/output/DHS/Tanzania 2010 DHS (Cluster).txt', row.names = F)
 
 # Nigeria 2013
-nga13.dhs <- read.dta(path('NG')%&%'nghr6adt/NGHR6AFL.DTA', convert.factors = NA) %>%
+nga13.dhs <- read.dta(path('NG')%&%'nghr6adt/NGHR6AFL.DTA', convert.factors = FALSE) %>%
   subset(select = vars)
 names(nga13.dhs) <- names
 
@@ -198,7 +215,7 @@ write.table(nga13.dhs, 'data/output/DHS/Nigeria 2013 DHS (Household).txt', row.n
 write.table(cluster(nga13.dhs, T), 'data/output/DHS/Nigeria 2013 DHS (Cluster).txt', row.names = F)
 
 # Malawi 2010
-mwi10.dhs <- read.dta(path('MW')%&%'mwhr61dt/MWHR61FL.DTA', convert.factors = NA) %>%
+mwi10.dhs <- read.dta(path('MW')%&%'mwhr61dt/MWHR61FL.DTA', convert.factors = FALSE) %>%
   subset(select = vars)
 names(mwi10.dhs) <- names
 
@@ -212,7 +229,7 @@ write.table(mwi10.dhs, 'data/output/DHS/Malawi 2010 DHS (Household).txt', row.na
 write.table(cluster(mwi10.dhs, T), 'data/output/DHS/Malawi 2010 DHS (Cluster).txt', row.names = F)
 
 # Rwanda 2010
-rwa10.dhs <- read.dta(path('RW')%&%'rwhr61dt/RWHR61FL.DTA', convert.factors = NA) %>%
+rwa10.dhs <- read.dta(path('RW')%&%'rwhr61dt/RWHR61FL.DTA', convert.factors = FALSE) %>%
   subset(select = vars)
 names(rwa10.dhs) <- names
 
